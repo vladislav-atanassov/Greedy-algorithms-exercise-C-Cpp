3803cfaac80eb8d4ae5466e34e62f9c6220e5a93
Vladislav Atanassov <vlavalat@gmail.com>
2023-11-29 13:39:14 +0200

    ActivitySelectionProblem.cpp added

--- ActivitySelectionProblem.cpp
+++ ActivitySelectionProblem.cpp
@@ -1,20 +1,54 @@
 #include <iostream>
 #include <vector>
+#include <algorithm> // std::sort
 
-//TODO: Create solution to this problem
+// Optional to use: 
+using intPair = std::pair<int, int>;
 
-int compareActivities(const void *a, const void *b) 
+// Compare to achive ascending order and use it in std::sort
+bool comp(const intPair& a, const intPair& b)
 {
-
+    return a.second < b.second;
 }
 
-int selectActivities(std::vector<std::pair<int, int>> startFinishTimes) 
+int activitySelectionProblem(std::vector<intPair>& startFinishTimes) 
 {
+    // Sort in acsending order according to the finish times
+    std::sort(startFinishTimes.begin(), startFinishTimes.end(), comp);
 
+    int maxActivities = 0, currMaxFinishTime = 0;
+
+    for(const auto& Pair : startFinishTimes)
+    {
+        // If the start time is later than the current finish time
+        // the activity does not overlap another one so it can be performed
+        if(Pair.first >= currMaxFinishTime) 
+        {
+            std::cout << "[" << Pair.first << ", " << Pair.second << "] ";
+
+            currMaxFinishTime = Pair.second; // assign to the new finish time
+            maxActivities++;
+        }
+    }
+
+    std::cout << "\nThe max number of activities is: " << maxActivities << std::endl;
+
+    return maxActivities;
 }
+
+/*
+// Example input
 
 int main() 
 {
+    std::vector<intPair> startFinishTimes = {{5, 9}, {1, 2}, {0, 6}, {5, 7}, {8, 9}, {3, 4}};
+
+    activitySelectionProblem(startFinishTimes); 
+    
+    // Expected output: 
+    // [1, 2] [3, 4] [5, 7] [8, 9]                                            
+    // The max number of activities is: 4
 
     return 0;
 }
+*/
--- KnapSackProblem.cpp
+++ KnapSackProblem.cpp
@@ -2,18 +2,21 @@
 #include <vector>
 #include <algorithm>
 
-bool comp(const std::pair<int, int>& a, const std::pair<int, int>& b)
+// Optional to use: 
+using intPair = std::pair<int, int>;
+
+bool comp(const intPair& a, const intPair& b)
 {
     return static_cast<double>(a.first / a.second > b.first / b.second);
 }
 
-int knapSackProblem(std::vector<std::pair<int, int>> valueWeightList, int capacity)
+int knapSackProblem(std::vector<intPair>& valueWeightList, int capacity)
 {
     std::sort(valueWeightList.begin(), valueWeightList.end(), comp);
 
     int counter = 0;
 
-    for(std::pair Pair : valueWeightList)
+    for(const auto& Pair : valueWeightList)
     {
         if(Pair.second <= capacity)
         {
@@ -30,7 +33,7 @@ int knapSackProblem(std::vector<std::pair<int, int>> valueWeightList, int capaci
 
 int main()
 {
-    std::vector<std::pair<int, int>> valueWeightList = {{100, 20}, {60, 10}, {120, 30}};
+    std::vector<intPair> valueWeightList = {{100, 20}, {60, 10}, {120, 30}};
     int capacity = 50;
 
     int maxVal = knapSackProblem(valueWeightList, capacity); // Expected output is 160
